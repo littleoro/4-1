369c43950d2fce5ac61ae20e670b28ae0f27e42e
littleoro <101625136+littleoro@users.noreply.github.com>
2022-04-01 17:51:23 +0800

    Add files via upload

---
+++ 4-1/4-1/4-1.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARININGS 1
+#include<stdio.h>
+#include<string.h>
+
+
+//√∞≈›≈≈–Ú
+//void bubble_sort(int *arr,int size) {
+//	int i= 0;
+//	for (i = 0; i < size - 1;i++) {
+//		int j = 0;
+//		for (j = 0; j < size-1-i;j++) {
+//			if (arr[j] > arr[j+1]) {
+//				int a = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = a;
+//			}
+//		}
+//	}
+//
+//}
+//
+//int main() {
+//	int arr[] = { 9,8,7,6,5,4,3,2,1,0 };
+//	int size= sizeof(arr) / sizeof(arr[0]);
+//	bubble_sort(arr,size);
+//	int i = 0;
+//	int n = sizeof(arr) / sizeof(arr[0]);
+//	for (i = 0; i < n; i++) {
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
+
+//»˝◊”∆Â
